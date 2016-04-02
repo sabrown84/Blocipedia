18a85579b7b9eafcd0eaf2e476e38ba7bf363b00
Sharrell Porter <SharrellPorter@example.com>
2016-04-02 13:47:47 -0400

    wiki-CRUd

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -15,6 +15,11 @@ RSpec.describe WikisController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "assigns [my_wiki] to @wikis" do
+      get :index
+      expect(assigns(:wikis)).to eq([my_wiki])
+    end
   end
 
   describe "GET #show" do
@@ -29,7 +34,33 @@ RSpec.describe WikisController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
   end
+
+  describe "POST create" do
+    it "increases the number of Wiki by 1" do
+      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Wiki,:count).by(1)
+    end
+
+    it "assigns the new wiki to @wiki" do
+        wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        expect(assigns(:wiki)).to eq Wiki.last
+      end
+
+      it "redirects to the new wiki" do
+        wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        expect(response).to redirect_to Wiki.last
+      end
+    end
 
   describe "GET #edit" do
     it "returns http success" do
