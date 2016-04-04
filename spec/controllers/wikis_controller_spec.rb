c2fbf13b920ccabc8dc28352504d44763a0544c9
Sharrell Porter <SharrellPorter@example.com>
2016-04-04 09:50:21 -0400

    wiki-CRUd

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -46,22 +46,6 @@ RSpec.describe WikisController, type: :controller do
     end
   end
 
-  describe "POST create" do
-    it "increases the number of Wiki by 1" do
-      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Wiki,:count).by(1)
-    end
-
-    it "assigns the new wiki to @wiki" do
-        wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(assigns(:wiki)).to eq Wiki.last
-      end
-
-      it "redirects to the new wiki" do
-        wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(response).to redirect_to Wiki.last
-      end
-    end
-
   describe "GET #edit" do
     it "returns http success" do
       get :edit, {id: my_wiki.id}
