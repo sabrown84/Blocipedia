2ef338671575d7f970429b63e5cd221e9044bb98
Sharrell Porter <SharrellPorter@example.com>
2016-04-26 17:42:23 -0400

    upgrade-account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,14 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+
+before_action :authenticate_user!
+
+
+  def downgrade
+    current_user.update_attribute(:role, 'standard_user')
+    flash[:notice] = "Account downgraded"
+
+    redirect_to (root_path)
   end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
-    else
-      render "new"
-    end
-  end
-  
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    authorize user
-    user.destroy
-    redirect_to users_path, :notice => "User deleted."
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :wikis
   resources :charges, only: [:new, :create]
 
-
+  get 'downgrade' => 'users#downgrade'
+  
   get 'about' => 'welcome#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
