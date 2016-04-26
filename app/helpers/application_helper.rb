32ab0cb93a2fbf4eef50a0a9f74d73b92beee30a
Sharrell Porter <SharrellPorter@example.com>
2016-04-26 15:46:30 -0400

    markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, exentsions)
+    (redcarpet.render markdown).html_safe
+  end
+  
 end
