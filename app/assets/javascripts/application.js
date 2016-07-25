f68a29424ad67e4760a42b0d91ae94757deeb979
Sharrell Porter <SharrellPorter@example.com>
2016-07-24 21:01:59 -0400

    fix epideditor1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require epiceditor/epiceditor
+//= require 'epiceditor'  
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount EpicEditor::Engine => "/"
 
   devise_for :users
 
